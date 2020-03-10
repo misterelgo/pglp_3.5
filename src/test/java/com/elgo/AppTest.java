354d4d0d74b44ea0e4366e9c13d561137eba3fd9
misterelgo <misterelgo@gmail.com>
2020-03-10 17:04:14 +0000

    Checkstyle avec ficher xml et mvn site

--- src/test/java/com/elgo/AppTest.java
+++ src/test/java/com/elgo/AppTest.java
@@ -13,7 +13,7 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void test()
     {
        ClasseMetier metier = new ClasseMetier();
         metier.MethodMetier();    }
